880762e4061de71d1ddbbec21ca251d3df43a43b
hkyo89 <hkyo89@yahoo.co.jp>
2017-07-21 14:07:17 +0700

    title

--- runtest.sh
+++ runtest.sh
@@ -8,12 +8,19 @@ mkdir $TMP
 
 echo "Bandwidth Test"
 echo "=============="
+echo ""
 
-echo "'git clone' test"
+echo ""
+echo "***** 1/3 'git clone' test ************************"
+echo ""
+
 GIT=`which git`
 time git clone https://github.com/hkyo89/bandwidth-test-github $TMP/github
 
-echo "'yarn add' test"
+echo ""
+echo "***** 2/3 'yarn add' test *************************"
+echo ""
+
 git clone https://github.com/hkyo89/bandwidth-test-yarn $TMP/yarn
 cd $TMP/yarn
 YARN=`which yarn`
@@ -21,7 +28,10 @@ $YARN cache clean
 time $YARN
 cd $BASEDIR
 
-echo "'curl' test"
+echo ""
+echo "***** 3/3 'curl' test *****************************"
+echo ""
+
 time curl https://releases.hashicorp.com/vagrant/1.9.7/vagrant_1.9.7_x86_64.dmg -o $TMP/vagrant.dmg
 
 rm -rf $TMP
